d497afa017f5ef3cc4bb71ab06aa74a8ef237772
Dominik Chrastecky <dominik@chrastecky.cz>
2025-08-31 21:12:17 +0200

    Load services

--- src/DependencyInjection/RikudouMatrixNotifierExtension.php
+++ src/DependencyInjection/RikudouMatrixNotifierExtension.php
@@ -2,13 +2,18 @@
 
 namespace Rikudou\MatrixNotifier\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class RikudouMatrixNotifierExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yaml');
+
         $configuration = $this->processConfiguration(new Configuration(), $configs);
 
         $container->setParameter('rikudou.internal.matrix.database_path', $configuration['database_path']);
